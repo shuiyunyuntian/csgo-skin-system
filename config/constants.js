b2c5cdd59d147087fc653a2ac70ff24c0a3093ec
shuiyunyuntian <116809810+shuiyunyuntian@users.noreply.github.com>
2026-03-03 20:07:37 +0800

    Create config/constants.js for application constants

---
+++ config/constants.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Application Constants
+const APPLICATION_NAME = 'CSGO Skin System';
+const VERSION = '1.0.0';
+const API_BASE_URL = 'https://api.csgoskins.com/v1';
+
+module.exports = {
+    APPLICATION_NAME,
+    VERSION,
+    API_BASE_URL,
+};
